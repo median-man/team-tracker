6838d0d1b1b9b056498ce65fb5b3cd2363daf5f7
John Desrosiers <john.desro@gmail.com>
2022-02-10 08:28:04 -0800

    Initial models files

---
+++ models/index.js
@@ -0,0 +1,12 @@
+const Member = require("./Member");
+const Note = require("./Note");
+const Team = require("./Team");
+const User = require("./User");
+
+// TODO: User:Team 1:* relationship
+
+// TODO:  Team:Note 1:* relationship
+
+// TODO: Team:Member 1:* relationship
+
+module.exports = { Member, Note, Team, User };
