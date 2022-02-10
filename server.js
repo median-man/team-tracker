6c5b4193089533bb4eecb379200428e43c556543
John Desrosiers <john.desro@gmail.com>
2022-02-10 04:37:34 -0800

    Configure hbars and sequelize

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
-console.log("I no worky! 😵");
+const express = require("express");
+const sequelize = require("./config/sequelize");
+const { engine } = require("express-handlebars");
+
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Use handlebars for rendering html
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", "./views");
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+async function start() {
+  try {
+    await sequelize.sync();
+    app.listen(PORT, () => console.log(`🌎 App listening on port ${PORT}`));
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+}
+start();
