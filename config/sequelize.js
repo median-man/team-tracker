b035387c0d3060cedcf97a35a99d3586455db351
John Desrosiers <john.desro@gmail.com>
2022-02-10 13:53:54 -0800

    Configure express app and sequelize

---
+++ config/sequelize.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+
+// configure database connection
+const { Sequelize } = require("sequelize");
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PW,
+  {
+    host: "localhost",
+    dialect: "mysql",
+  }
+);
+
+module.exports = sequelize;
--- server.js
+++ server.js
@@ -1,1 +1,28 @@
-console.log("I no worky! 😵");
+const express = require("express");
+const sequelize = require("./config/sequelize");
+const { engine } = require("express-handlebars");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// setup static middleware to serve js, css, images, and other files
+app.use(express.static("public"));
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", "./views");
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+(async () => {
+  try {
+    await sequelize.sync();
+    app.listen(PORT, () => {
+      console.log(`🚀 Server listening on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+})();
