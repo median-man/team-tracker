ecbf53adeebe146de0800e12748d8481c8722bca
John Desrosiers <john.desro@gmail.com>
2022-02-10 08:13:25 -0800

    Add static signup view

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
 app.set("views", "./views");
 
+app.use(express.static("public"));
+
 app.use(routes);
 
 async function start() {
