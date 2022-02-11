d9d696b57525a519f14128b843132ad6ae2e5346
John Desrosiers <john.desro@gmail.com>
2022-02-11 08:50:49 -0800

    Define Team model and Team-User assocaition

--- models/index.js
+++ models/index.js
@@ -3,7 +3,16 @@ const Note = require("./Note");
 const Team = require("./Team");
 const User = require("./User");
 
-// TODO: User:Team 1:* relationship
+// User's have many teams and a team MUST belong to one user.
+User.hasMany(Team, {
+  foreignKey: {
+    // default fk is userId
+    allowNull: false,
+  },
+  // Delete the teams if the user is deleted
+  onDelete: "CASCADE",
+});
+Team.belongsTo(User);
 
 // TODO:  Team:Note 1:* relationship
 
