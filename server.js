c27bb0f7206ce3c092147c02df1164b4246d125e
John Desrosiers <john.desro@gmail.com>
2022-02-14 06:26:02 -0800

    Setup session middleware

--- server.js
+++ server.js
@@ -1,10 +1,34 @@
 const express = require("express");
+const session = require("express-session");
+// initialize sequelize with session store
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const sequelize = require("./config/sequelize");
 const { engine } = require("express-handlebars");
 const routes = require("./controllers");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// configure session middleware
+app.use(
+  session({
+    secret: "keyboard cat",
+    cookie: {
+      // expires after 24 hours
+      maxAge: 24 * 60 * 60 * 1000,
+      // only use cookie for login session (no client side data)
+      httpOnly: true,
+      // TODO: use process.env.NODE_ENV to conditionally set cookie to secure mode
+    },
+    store: new SequelizeStore({
+      db: sequelize,
+    }),
+    resave: false, // we support the touch method so per the express-session docs this should be set to false
+    proxy: true, // node server sits behind a Heroku proxy when deployed
+    secure: false,
+    saveUninitialized: false,
+  })
+);
 
 // Use handlebars for rendering html
 app.engine("handlebars", engine());
