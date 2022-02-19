d1336cfa064a7561efda474bf929ddbe44d6e4c5
John Desrosiers <john.desro@gmail.com>
2022-02-19 12:55:37 -0800

    Create team controller adds team to db

--- controllers/teams.js
+++ controllers/teams.js
@@ -1,7 +1,25 @@
+const { Team } = require("../models");
+
 module.exports = {
   // 	create new team
-  createTeam: (req, res) => {
-    res.json({ message: "TODO: create team" });
+  createTeam: async (req, res) => {
+    try {
+      if (!req.session.userId) {
+        return res
+          .status(401)
+          .json({ message: "Unauthorized. Login required." });
+      }
+      const team = await Team.create({
+        title: req.body.title,
+        userId: req.session.userId,
+      });
+      res.json({ message: "Created team", team });
+    } catch (error) {
+      console.error(error);
+      res
+        .status(400)
+        .json({ message: "An error occurred trying to create a team." });
+    }
   },
 
   // delete team (including notes and members)
