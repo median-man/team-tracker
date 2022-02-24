a53e1c673a1685898c0b7bff02b8b7f6a3a740e6
John Desrosiers <john.desro@gmail.com>
2022-02-23 18:00:15 -0800

    Add forbiddenError to errors.js

--- controllers/errors.js
+++ controllers/errors.js
@@ -4,4 +4,10 @@ module.exports = {
       message: "Team doesn't exist or user is not the team owner.",
     }),
   serverError: (res) => res.status(500).json({ message: "Server error" }),
+  forbiddenError: (res) =>
+    res
+      .status(403)
+      .json({
+        message: "User does not have permission for the requested resource.",
+      }),
 };
