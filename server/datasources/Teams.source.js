7782b9ad076d5380dd023b5f4ca645437f07bb49
John Desrosiers <john.desro@gmail.com>
2022-03-29 08:14:30 -0700

    Refactor TeamsSource datasource
    Remove direct references to the Team model. Instead, use this withing
    the class to ensure the Datasource can batch efficiently.

--- server/datasources/Teams.source.js
+++ server/datasources/Teams.source.js
@@ -1,22 +1,21 @@
 const { MongoDataSource } = require("apollo-datasource-mongodb");
-const Team = require("../models/Team");
 
 class TeamsSource extends MongoDataSource {
   create({ userId, ...values }) {
-    return Team.create({ ...values, user: userId });
+    return this.model.create({ ...values, user: userId });
   }
   findByUserId(userId) {
     return this.model.find({ user: userId });
   }
   addMember({ memberName, teamId, userId }) {
-    return Team.findOneAndUpdate(
+    return this.model.findOneAndUpdate(
       { _id: teamId, user: userId },
       { $addToSet: { members: memberName } },
       { new: true }
     );
   }
   removeMember({ memberName, teamId, userId }) {
-    return Team.findOneAndUpdate(
+    return this.model.findOneAndUpdate(
       { _id: teamId, user: userId },
       { $pull: { members: memberName } },
       { new: true }
