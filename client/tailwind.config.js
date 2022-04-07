13ce1de2b6c98b6e300f350d1e90a3a123568fc6
John Desrosiers <john.desro@gmail.com>
2022-04-07 08:20:09 -0700

    Add colors to tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,16 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: ["./src/**/*.{js,jsx}"],
   theme: {
+    colors: {
+      transparent: "transparent",
+      white: colors.white,
+      blue: colors.sky,
+      red: colors.red,
+      gray: colors.neutral,
+    },
     extend: {},
   },
   plugins: [],
-}
+};
