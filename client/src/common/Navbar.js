f740dcfa57c5f966459b52e8f35460569c9795ce
John Desrosiers <john.desro@gmail.com>
2022-04-08 07:57:08 -0700

    Update Navbar component - responsive collapse
    Navbar uses the useBreakpoint hook to force open/close the collapsed
    navigation menu using md breakpoint.

--- client/src/common/Navbar.js
+++ client/src/common/Navbar.js
@@ -1,8 +1,9 @@
 import { NavLink } from "react-router-dom";
 import { MenuIcon } from "@heroicons/react/solid";
 import { useAuth } from "../util/auth";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Collapse } from "./Collapse";
+import { useBreakpoint } from "../util/useBreakpoint";
 
 // Classes shared by buttons and links in the navigation menu
 const navItemClasses =
@@ -19,16 +20,31 @@ function LogoutButton() {
 
 export default function Navbar() {
   const { isLoggedIn } = useAuth();
-  const [isMenuOpen, setIsMenuOpen] = useState(true);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const toggleMenu = () => setIsMenuOpen((isMenuOpen) => !isMenuOpen);
+  
+  // open menu when screen is wider than md breakpoint and close menu when
+  // screen is resized smaller than md breakpoint
+  const keepMenuOpen = useBreakpoint("md");
+  useEffect(() => {
+    if (keepMenuOpen) {
+      setIsMenuOpen(true)
+    } else {
+      setIsMenuOpen(false);
+    }
+  }, [keepMenuOpen]);
 
   return (
     <header className="bg-blue-500 text-white flex justify-between items-center flex-wrap md:min-h-full md:block">
       <h1 className="text-2xl font-medium pb-2 py-2 px-3 grow">Team Tracker</h1>
-      <button onClick={toggleMenu} className="h-6 w-6 mr-3 grow-0 md:hidden" aria-label="toggle navigation menu">
+      <button
+        onClick={toggleMenu}
+        className="h-6 w-6 mr-3 grow-0 md:hidden"
+        aria-label="toggle navigation menu"
+      >
         <MenuIcon />
       </button>
-      <Collapse className="w-full" open={isMenuOpen}>
+      <Collapse className="w-full" open={isMenuOpen} breakpoint="md">
         <nav className={`flex flex-col pb-4`}>
           {isLoggedIn ? (
             <>
