235b8bb03999c36f14d6782c35803a5a4b3f9128
John Desrosiers <john.desro@gmail.com>
2022-04-10 15:05:24 -0700

    Update Signup component - loading and transitions
    The Signup component displays a loading message.
    
    The Signup component uses fade in and fade out transitions.
    
    Add test case for loading message.

--- client/src/landing/Signup.jsx
+++ client/src/landing/Signup.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { Fade } from "./Fade";
 import {
   Divider,
   Label,
@@ -8,47 +10,59 @@ import {
   SubmitButton,
   LinkButton,
 } from "./Form";
+import { LoadingAlert } from "./LoadingAlert";
 import { Wrapper } from "./Wrapper";
 
 export function Signup() {
+  const [showLoadingAlert, setShowLoadingAlert] = useState(false);
+
+  // Fade out form on submit
   const handleFormSubmit = (event) => {
     event.preventDefault();
+    // setShowFade(false);
+    setShowLoadingAlert(true);
   };
   return (
     <Wrapper>
-      <Form onSubmit={handleFormSubmit}>
-        <Heading>Create an Account</Heading>
-        <p className="pb-8">
-          Creating an account enables you to start tracking the progress of your
-          teams and keep a log.
-        </p>
-        <Card>
-          <Label htmlFor="username">Username</Label>
-          <Input
-            type="text"
-            name="username"
-            id="username"
-            placeholder="create a username"
-          />
-          <Label htmlFor="email">Email</Label>
-          <Input
-            type="email"
-            name="email"
-            id="email"
-            placeholder="enter your email"
-          />
-          <Label htmlFor="password">Password</Label>
-          <Input
-            type="password"
-            name="password"
-            id="password"
-            placeholder="enter a password"
-          />
-          <SubmitButton>Create account</SubmitButton>
-          <Divider>Already have an account?</Divider>
-          <LinkButton to="/login">Sign in</LinkButton>
-        </Card>
-      </Form>
+      <Fade>
+        {showLoadingAlert ? (
+          <LoadingAlert message="Creating your account..." />
+        ) : (
+          <Form onSubmit={handleFormSubmit}>
+            <Heading>Create an Account</Heading>
+            <p className="pb-8">
+              Creating an account enables you to start tracking the progress of
+              your teams and keep a log.
+            </p>
+            <Card>
+              <Label htmlFor="username">Username</Label>
+              <Input
+                type="text"
+                name="username"
+                id="username"
+                placeholder="create a username"
+              />
+              <Label htmlFor="email">Email</Label>
+              <Input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="enter your email"
+              />
+              <Label htmlFor="password">Password</Label>
+              <Input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="enter a password"
+              />
+              <SubmitButton>Create account</SubmitButton>
+              <Divider>Already have an account?</Divider>
+              <LinkButton to="/login">Sign in</LinkButton>
+            </Card>
+          </Form>
+        )}
+      </Fade>
     </Wrapper>
   );
 }
--- client/src/landing/Signup.test.js
+++ client/src/landing/Signup.test.js
@@ -1,16 +1,55 @@
-import { BrowserRouter as Router } from "react-router-dom";
-import { render } from "@testing-library/react";
+import { fireEvent, render, screen } from "../util/testUtils";
+import userEvent from "@testing-library/user-event";
+import { graphql } from "msw";
+import { setupServer } from "msw/node";
 import { Signup } from "./Signup";
 
+const server = setupServer(
+  graphql.mutation("CreateUser", (req, res, ctx) =>
+    res(
+      ctx.data({
+        createUser: {
+          user: {
+            _id: "6252cee3fe89582ec32525ef",
+            email: req.variables.email,
+            username: req.variables.username,
+          },
+        },
+      })
+    )
+  )
+);
+
+beforeAll(() => server.listen());
+afterAll(() => server.close());
+
 test("renders a Signup component", () => {
-  render(
-    <Router>
-      <Signup />
-    </Router>
-  );
+  render(<Signup />);
 });
 
-test.todo("displays a spinner while loading");
+test("displays a loading indicator after user clicks submit", async () => {
+  render(<Signup />);
+
+  // fake values for testing
+  const username = "sandsOfArrakis";
+  const email = "paul@houseatreides.net";
+  const password = "mu@dD1b";
+
+  // type in form values
+  userEvent.type(screen.getByRole("textbox", { name: /username/i }), username);
+  userEvent.type(screen.getByRole("textbox", { name: /email/i }), email);
+  userEvent.type(screen.getByLabelText(/password/i), password);
+
+  userEvent.click(screen.getByRole("button", { name: /create account/i }));
+
+  // manually fire transitionEnd (testing library doesn't simulate this)
+  fireEvent.transitionEnd(await screen.findByTestId("fade"));
+
+  const loadingMessage = await screen.findByRole("alert");
+  expect(loadingMessage).toHaveTextContent(/creating your account/i);
+  expect(loadingMessage).toBeVisible();
+});
+
 test.todo("user cannot submit while loading");
 test.todo("redirects after successful login");
 test.todo("displays an alert message when login fails");
