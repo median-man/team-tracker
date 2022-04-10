ad48c2e0481511ea5bb2491f31672db24d0fc31c
John Desrosiers <john.desro@gmail.com>
2022-04-10 04:12:30 -0700

    Add form style and layout to landing/Signup
    Create static styled Sign Up form.

--- client/src/landing/Signup.jsx
+++ client/src/landing/Signup.jsx
@@ -1,3 +1,54 @@
+import {
+  Divider,
+  Label,
+  Input,
+  Form,
+  Heading,
+  Card,
+  SubmitButton,
+  LinkButton,
+} from "./Form";
+import { Wrapper } from "./Wrapper";
+
 export function Signup() {
-  return <h1>Signup</h1>;
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <Wrapper>
+      <Form onSubmit={handleFormSubmit}>
+        <Heading>Create an Account</Heading>
+        <p className="pb-8">
+          Creating an account enables you to start tracking the progress of your
+          teams and keep a log.
+        </p>
+        <Card>
+          <Label htmlFor="username">Username</Label>
+          <Input
+            type="text"
+            name="username"
+            id="username"
+            placeholder="create a username"
+          />
+          <Label htmlFor="email">Email</Label>
+          <Input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="enter your email"
+          />
+          <Label htmlFor="password">Password</Label>
+          <Input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="enter a password"
+          />
+          <SubmitButton>Create account</SubmitButton>
+          <Divider>Already have an account?</Divider>
+          <LinkButton to="/login">Sign in</LinkButton>
+        </Card>
+      </Form>
+    </Wrapper>
+  );
 }
